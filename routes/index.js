3069b680e874b71d5f650ba07f7ddf4749fe9693
Sujith Bheemireddi <sujithterminal@gmail.com>
2015-04-05 10:29:23 -0400

    Correcting an endpoint

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,7 @@ router.get('/', function(req, res) {
 });
 
 router.get('/incoming', function(req, res) {
-  res.sendFile(path.join(__dirname, '../public', 'twilml/incoming.xml'));
-});
-
-router.get('/process', function(req, res) {
-	console.log('Request received to process: ' + req.query.Digits);
-	var option = req.query.Digits;
-	res.sendFile(path.join(__dirname, '../public', 'twilml/capco.xml'));
-	
+  res.sendFile(path.join(__dirname, '../public', 'twilml/callind.xml'));
 });
 
 module.exports = router;
